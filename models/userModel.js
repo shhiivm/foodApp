4e9099e866fc83c76e381eb45614c6e53cccfab3
shhiivm <shhiivm@pm.me>
2025-10-02 23:02:15 +0530

    user schema created

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    emsil: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      require: [true, "phone number is required"],
+    },
+    usertype: {
+      type: String,
+      default: "client",
+      enums: ["client", "admin", "vendor", "driver"],
+    },
+    profile: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2025/08/28/11/47/user-9801866_1280.png",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", userSchema);
